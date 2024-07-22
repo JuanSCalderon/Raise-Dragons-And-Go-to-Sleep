896120454c9ab963ee1751cc13447cf587f77aec
Brayan Alejandro Perea <brayanperea314@gmail.com>
2024-07-22 13:00:36 -0500

    Script PlayerController para mover alrededor de la pantalla
    Se realizó el primero script para generar el movimiento en pantalla del jugador

--- Assets/Scripts/PayerController.cs
+++ Assets/Scripts/PayerController.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class PayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-
-
+    public float speed;
+    private Vector2 moveAxis;
+    private Vector2 moveDir;
+    private Rigidbody2D rb;
+    
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Capturar entrada del jugador en los ejes horizontales y verticales
+        moveAxis = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        
+        // Normalizar el vector de movimiento si su magnitud es mayor que 1
+        if (moveAxis.magnitude > 1)
+        {
+            moveAxis.Normalize();
+        }
+        
+        // Multiplicar por la velocidad
+        moveDir = moveAxis * speed;
+    }
 
+    void FixedUpdate()
+    {
+        // Mover el objeto usando el Rigidbody2D para manejar la física
+        rb.velocity = moveDir * Time.fixedDeltaTime;
     }
 }
+
+
