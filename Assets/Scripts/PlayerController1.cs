f2a8a847f31b21b7f8f6efeb6ad045f0b73c495c
Brayan Alejandro Perea <brayanperea314@gmail.com>
2024-07-22 15:25:15 -0500

    Follow Player Script realizado
    Se ha hecho el follow player con delimitaciones de terreno para que no se salga del mapa, aun falta las delimitaciones del jugador

---
+++ Assets/Scripts/PlayerController1.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController1 : MonoBehaviour
+{
+  public float speed;
+    private Vector2 moveAxis;
+    private Vector2 moveDir;
+    private Rigidbody2D rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Capturar entrada del jugador en los ejes horizontales y verticales
+        moveAxis = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        
+        // Normalizar el vector de movimiento si su magnitud es mayor que 1
+        if (moveAxis.magnitude > 1)
+        {
+            moveAxis.Normalize();
+        }
+        
+        // Multiplicar por la velocidad
+        moveDir = moveAxis * speed;
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = moveDir * Time.fixedDeltaTime;
+    }
+}
---
+++ Assets/Scripts/PlayerFollow.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour
+{
+    public GameObject player; 
+    private float targetPoseX;
+    private float targetPoseY;
+    private float posX;
+    private float posy;
+    public float dereMax;
+    public float izqMax;
+    public float alturaMax;
+    public float alturaMin;
+    public float speed;
+    public bool encendida = true;
+    void Awake() {
+        posX= targetPoseX + dereMax;
+        posy = targetPoseY + alturaMin;
+        transform.position= Vector3.Lerp(transform.position, new Vector3(posX,posy,-1),1);
+    }
+    void MoveCam(){
+        if(encendida){
+            if(player){
+                targetPoseX= player.transform.position.x;
+                targetPoseY= player.transform.position.y;
+                if(targetPoseX> dereMax && targetPoseX < izqMax){
+                    posX= targetPoseX;
+                }
+                if(targetPoseY < alturaMax && targetPoseY > alturaMin){
+                    posy= targetPoseY;
+                }
+            }
+            transform.position = Vector3.Lerp(transform.position, new Vector3(posX, posy,-1),speed*Time.deltaTime);
+        }
+    }
+    void LateUpdate()
+    {
+        MoveCam();
+    }
+}
+
+
