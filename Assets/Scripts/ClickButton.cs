773d854f6d66e56076762cfa31eaa48232944f91
Juan Sebastián Calderón <82604620+JuanSCalderon@users.noreply.github.com>
2024-07-23 09:39:28 -0500

    Finishing details of UI, New Assets (buttons, banners, animations)

--- Assets/Scripts/ClickButton.cs
+++ Assets/Scripts/ClickButton.cs
@@ -4,28 +4,35 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ClickButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+public class ClickButton : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] private Image _img;
     [SerializeField] private Sprite _default, _pressed;
     [SerializeField] private AudioClip _compressClip, _uncompressClip;
     [SerializeField] private AudioSource _source;
 
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        _img.sprite = _pressed;
-        _source.PlayOneShot(_compressClip);
-    }
+    private bool _isPressed = false;
 
-    public void OnPointerUp(PointerEventData eventData)
+    public void OnPointerClick(PointerEventData eventData)
     {
-        _img.sprite = _default;
-        _source.PlayOneShot(_uncompressClip);
+        _isPressed = !_isPressed;
+
+        if (_isPressed)
+        {
+            _img.sprite = _pressed;
+            _source.PlayOneShot(_compressClip);
+        }
+        else
+        {
+            _img.sprite = _default;
+            _source.PlayOneShot(_uncompressClip);
+        }
+
+        Clicked();
     }
 
     public void Clicked()
     {
-        Debug.Log("Perfom Action clicked");
+        Debug.Log("Perform Action clicked");
     }
-
 }
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Nombre de la música del menú principal y la música del juego
+    public string mainMenuMusic = "MainMenuMusic";
+    public string gameMusic = "GameMusic";
+    public string sFxStart = "StartButton";
+
+    private void Start()
+    {
+        // Reproducir la música del menú principal al inicio
+        AudioManager.Instance.PlayMusic(mainMenuMusic);
+    }
+
+    public void PlayGame()
+    {
+        // Cambiar la música cuando se inicie el juego
+        SceneManager.LoadSceneAsync(1);
+        AudioManager.Instance.PlaySFX(sFxStart);
+        AudioManager.Instance.PlayMusic(gameMusic);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
